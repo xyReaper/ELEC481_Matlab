fb54f7f5b8c9596a1d5a7302df912f16b28d34b3
xyReaper <rami@spaceconcordia.ca>
2016-11-18 19:54:00 -0500

    Added Controllable

--- System_Identification/step2.m
+++ System_Identification/step2.m
@@ -1,3 +1,4 @@
+clear
 zetad32 = 0.0133;
 zetad12 = 0.1216;
 Jm = 0.0162;
@@ -14,15 +15,13 @@ Ds = [J1*J2 (c1*J2 + c2*J1) J1*(k1+k2)+J2*k1+c1*c2 c1*(k1+k2)+c2*k1 k1*k2];
 
 TF1 = tf(khw.*[J2 c2 k1+k2],Ds);
 TF2 = tf(khw.*[0 0 k1], Ds);
-
-figure
-subplot(2,1,1)
-pzmap(TF1)
-grid on
-
-subplot(2,1,2)
-pzmap(TF2)
-grid on
-
-
-
+TF3 = tf([0 0 k1], [J2 c2 k1+k2]);
+% 
+% figure
+% subplot(2,1,1)
+% pzmap(TF1)
+% grid on
+% 
+% subplot(2,1,2)
+% pzmap(TF2)
+% grid on
